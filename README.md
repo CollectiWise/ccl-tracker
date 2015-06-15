8cee0949d08049c26416b53dc524e6c82aa0bfaa
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-06-15 14:54:54 +0200

    Adding fireIncrementalEvent support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ The CCL-Tracker javascript library is used as the middleware "glue" between a ja
 
 In addition, it provides the necessary abstraction between the application developer and the analytics responsible, allowing the latter to chose the most appropriate tracking solution, even at a later time.
 
-## Usage
+## 1. Usage
 
 To use it in your project, just include the library in your header:
 
@@ -28,7 +28,7 @@ Or even directly in your DOM:
 <a href="#" onmouseover="analytics.fireEvent('mouseover')">This is a link</a>
 ```
 
-## AMD Module
+## 2. AMD Module
 
 The library also supports AMD module loading when available. To use it in your own project, just include it the same way you load an AMD module:
 
@@ -50,9 +50,51 @@ define(["jquery", "extern/analytics"], function($, analytics) {
 
 ```
 
-## API Reference
+## 3. Connecting to analytics back-end
+
+As we mentioned before, this library is just the middleware between the analytics back-end and a higher-level front-end. Without any analytics back-end this library is useless.
+
+In order to receive analytics events, you will have to define the global property `analyticsListener` at any time during or after the page loading.
+
+There are two ways to receive events: Using a callback, or using window DOM events.
+
+**IMPORTANT:** If you do not define the `analyticsListener` property within 10 seconds after the page is fully loaded, the ccl-tracker library will automatically disable the entire analytics mechanism. 
+
+### Using a Callback
+
+If you want to have just a single point where you receive all the analytics events, you should define a callback listener. To do so, just declare a global function named `analyticsListener`. Your function will be fired when an analytics event has to be submitted to the server.
+
+For example:
+
+```javascript
+window.analyticsListener = function( eventName, eventData ) {
+    // Handle the event [eventName]
+    // The [eventData] object contain all the event properties
+    ...
+}
+```
+
+### Using DOM Events
+
+If you want to handle individual events, you can let the library forward each analytics action on the window DOM individually. To do so, set the global property `analyticsListener` to `true`. This will indicate the tracking library to fire a special event called `analytics.XXXXX`, where XXXXX is the event name.
+For example:
+
+```javascript
+window.analyticsListener = true;
+$(window).on('analytics.login', function(event, eventData) {
+    // Handle the event 'login'
+    // The [eventData] object contain all the event properties
+    ...
+});
+```
+
+## 4. API Reference
 
 The `analytics` instance has the following functions:
+
+### 4.1. Event Functions
+
+The following functions trigger analytics events or control the way they behave.
 
 #### setGlobal( `name`, `value` )
 
@@ -76,6 +118,60 @@ Example:
 analytics.fireEvent('ui.click', {
     'button': 'submit',
     'id': 'ui-submit'
+});
+```
+
+#### fireIncrementalEvent( `name`, `properties`, `property`, [ `value` ] )
+
+Trigger an analytics event every time `property` reaches a particular interval. The `name` argument is the name of the event and the `properties` object contains all the properties this event will have.
+
+The `property` argument can be either a string or an object. In the first case it's considered to be the name of the property to monitor, assuming the interval is `1`, while on the second (recommended) has the following syntax:
+
+```javascript
+{
+    "property"      : "..", // The property to monitor
+    "eventProperty" : "..", // The event property to store the result
+    "interval"      : 1,    // The interval to which the event should be fired
+    "value"         : 0     // The current value of the property
+}
+```
+
+The value can either be passed as a property of the `property` object, or as a fourth, `value` argument.
+
+Example:
+
+```javascript
+analytics.fireIncrementalEvent('engagement.minutes', {
+    'interface': 'ui-login'
+}, {
+    'property'      : 'ui-login.engagement',
+    'eventProperty' : 'time',
+    'interval'      : 60
+},
+    analytics.getTimer('ui-login')
+);
+```
+
+The above example will fire the event `engagement.minutes` every time the `ui-login` timer reaches a round multiplicand of 60. For every event, the property `time` will be sent included in the event, containing the current value of the timer. 
+
+### 4.2. Timer Functions
+
+The following functions are for assistance in calculating time differences between checkpoints. This is particularly useful when you want to calculate how much time the user spent on something.
+
+Example
+
+```javascript
+// When the user focuses on the window, start the timer
+$(window).focus(function() {
+    analytics.startTimer('idle-time'); 
+});
+// When the user moves focus away, fire an event specifying
+// how much time the user spent on-line.
+$(window).blur(function() {
+    var offTime = analytics.stopTimer('idle-time');
+    analytics.fireEvent('user.online', {
+        'time': offLime
+    });
 });
 ```
 
@@ -119,40 +215,35 @@ Example:
 var interaction_state = analytics.getTimer('ui.interaction');
 ```
 
-## Connecting to analytics back-end
+### 4.3. Metrics Functions
 
-As we mentioned before, this library is just the middleware between the analytics back-end and a higher-level front-end. Without any analytics back-end this library is useless.
+The following functions can be used to calculate accumulated values.
 
-In order to receive analytics events, you will have to define the global property `analyticsListener` at any time during or after the page loading.
+#### accumulate( `name`, `value` )
 
-There are two ways to receive events: Using a callback, or using window DOM events.
+Accumulate the specified value to the accumulator with the given name and return the result.
 
-**IMPORTANT:** If you do not define the `analyticsListener` property within 10 seconds after the page is fully loaded, the ccl-tracker library will automatically disable the entire analytics mechanism. 
-
-### Using a Callback
-
-If you want to have just a single point where you receive all the analytics events, you should define a callback listener. To do so, just declare a global function named `analyticsListener`. Your function will be fired when an analytics event has to be submitted to the server.
-
-For example:
+Example:
 
 ```javascript
-window.analyticsListener = function( eventName, eventData ) {
-    // Handle the event [eventName]
-    // The [eventData] object contain all the event properties
-    ...
-}
+var hits = analytics.accumulate('ui.hits', 10); // Add 10 more hits
 ```
 
-### Using DOM Events
+#### clearAccum( `name` )
 
-If you want to handle individual events, you can let the library forward each analytics action on the window DOM individually. To do so, set the global property `analyticsListener` to `true`. This will indicate the tracking library to fire a special event called `analytics.XXXXX`, where XXXXX is the event name.
-For example:
+Remove the value of the accumulator with the specified name.
+
+#### delta( `name`, `value` )
+
+Calculate the delta between current `value` and last one stored from a previous call of this function and return it. 
+
+Example:
 
 ```javascript
-window.analyticsListener = true;
-$(window).on('analytics.login', function(event, eventData) {
-    // Handle the event 'login'
-    // The [eventData] object contain all the event properties
-    ...
-});
+// Count how many new jobs are processed
+var new_jobs = analytics.delta('vm.jobs', get_job_count());
 ```
+
+#### clearDelta( `name` )
+
+Remove last delta value for the item with the specified name.
--- src/analytics.js
+++ src/analytics.js
@@ -5,6 +5,15 @@
  * Pick an initiator function according to AMD or stand-alone loading
  */
 ( window.define == undefined ? function(fn) { window.analytics = fn(); } : window.define )(function() {
+	"use strict";
+
+	/**
+	 * Keys to localstore
+	 */
+	var KEY_ACCUMULATORS = "_ccl_tracking_accumulators",
+		KEY_ACCUMDELTA 	 = "_ccl_tracking_delta",
+		KEY_INCREMENTAL	 = "_ccl_tracking_incremental",
+		KEY_TRACKID 	 = "_ccl_tracking_id";
 
 	/**
 	 * Generate a tracking ID (An extension over GUID v4)
@@ -47,7 +56,7 @@
 
 		// Timers
 		this.timers = { };
-		this.timerAccummulators = { };
+		this.timerAccumulators = { };
 
 		// Global properties
 		this.globals = { };
@@ -56,23 +65,33 @@
 		this.probeTimer = setInterval(this.probeListener.bind(this), 100);
 
 		// Initialize or resume a tracking session
-		this.trackingID = this.getCookie("_ccl_tracking_id")
+		this.trackingID = this.getCookie( KEY_TRACKID );
+		if (!this.trackingID && localStorage)
+			this.trackingID = localStorage.getItem( KEY_TRACKID );
 		if (!this.trackingID) {
 			// Generate a tracking ID
 			this.trackingID = trackid();
-			// Store it as cookie
-			this.createCookie("_ccl_tracking_id", this.trackingID, 365);
+			// Store it as cookie & localStorage item
+			this.createCookie( KEY_TRACKID , this.trackingID, 365);
+			if (localStorage)
+				localStorage.setItem( KEY_TRACKID , this.trackingID);
 		}
 
 		// Include trackid as a parameter
 		this.globals['trackid'] = this.trackingID;
 
 	}
-	
+
+	/**
+	 * Assistance for javascript optimizer
+	 */
+	var localStorage = window.localStorage,
+		Analytics_prototype = Analytics.prototype;
+
 	/**
 	 * Create a cookie on the browser cookie store
 	 */
-	Analytics.prototype.createCookie = function(name, value, days) {
+	Analytics_prototype.createCookie = function(name, value, days) {
 		var expires;
 		if (days) {
 			var date = new Date();
@@ -88,7 +107,8 @@
 	/**
 	 * Fetch a cookie from browser cookie store
 	 */
-	Analytics.prototype.getCookie = function(c_name) {
+	Analytics_prototype.getCookie = function(c_name) {
+		var c_start, c_end;
 		if (document.cookie.length > 0) {
 			c_start = document.cookie.indexOf(c_name + "=");
 			if (c_start != -1) {
@@ -104,18 +124,9 @@
 	}
 
 	/**
-	 * Disable the analytics functionality entirely
-	 */
-	Analytics.prototype.disable = function() {
-		// Mark as disabled and expired
-		this.expired = true;
-		this.enabled = false;
-	}
-
-	/**
 	 * Trigger an analytics action
 	 */
-	Analytics.prototype.probeListener = function() {
+	Analytics_prototype.probeListener = function() {
 		// Check if we are enabled or expired
 		if (this.enabled || this.expired) return;
 
@@ -149,9 +160,40 @@
 	}
 
 	/**
+	 * Send the analytics event without the stack
+	 */
+	Analytics_prototype.send = function( eventName, data ) {
+
+		// Append timestamp if missing
+		if (data.ts == undefined)
+			data.ts = Date.now();
+
+		// Log seding actions
+		if (this.debug)
+			console.log("Analytics: Sending", eventName, data);
+
+		// Fire the event listener
+		if (this.listener) {
+			try {
+				// Backwards compatibility
+				if (this.listener === true) {
+					$(window).trigger('analytics.'+eventName, [data]);
+				} else {
+					// New version just calls the listener
+					this.listener(eventName, data);
+				}
+			} catch (e) { };
+		}
+	}
+
+	/* ############################################################ */
+	/*  Low-Level API Functions                                     */
+	/* ############################################################ */
+
+	/**
 	 * Trigger the analytics event
 	 */
-	Analytics.prototype.fireEvent = function( eventName, data, replace ) {
+	Analytics_prototype.fireEvent = function( eventName, data, replace ) {
 
 		// Check for listener
 		this.probeListener();
@@ -161,7 +203,7 @@
 
 		// Append globals
 		if (!data) data={};
-		for (k in this.globals)
+		for (var k in this.globals)
 			data[k] = this.globals[k];
 
 		// Forward or stack it
@@ -188,36 +230,9 @@
 	}
 
 	/**
-	 * Send the analytics event without the stack
-	 */
-	Analytics.prototype.send = function( eventName, data ) {
-
-		// Append timestamp if missing
-		if (data.ts == undefined)
-			data.ts = Date.now();
-
-		// Log seding actions
-		if (this.debug)
-			console.log("Analytics: Sending", eventName, data);
-
-		// Fire the event listener
-		if (this.listener) {
-			try {
-				// Backwards compatibility
-				if (this.listener === true) {
-					$(window).trigger('analytics.'+eventName, [data]);
-				} else {
-					// New version just calls the listener
-					this.listener(eventName, data);
-				}
-			} catch (e) { };
-		}
-	}
-
-	/**
 	 * Set a global property
 	 */
-	Analytics.prototype.setGlobal = function( name, value ) {
+	Analytics_prototype.setGlobal = function( name, value ) {
 		// Update global property
 		this.globals[name] = value;
 	}
@@ -225,18 +240,18 @@
 	/**
 	 * Freeze analytics timers
 	 */
-	Analytics.prototype.freeze = function() {
+	Analytics_prototype.freeze = function() {
 		// Snapshot all timers and place on accumulators
 		for (name in this.timers) {
 			// Collect duration till NOW on accummulators
-			this.timerAccummulators[name] += (Date.now() - this.timers[name]);
+			this.timerAccumulators[name] += (Date.now() - this.timers[name]);
 		}
 	}
 
 	/**
 	 * Thaw analytics timers
 	 */
-	Analytics.prototype.thaw = function() {
+	Analytics_prototype.thaw = function() {
 		// Restart all timers
 		for (name in this.timers) {
 			// Start counting from NOW
@@ -245,25 +260,38 @@
 	}
 
 	/**
+	 * Disable the analytics functionality entirely
+	 */
+	Analytics_prototype.disable = function() {
+		// Mark as disabled and expired
+		this.expired = true;
+		this.enabled = false;
+	}
+
+	/* ############################################################ */
+	/*  Timer API  Functions                                        */
+	/* ############################################################ */
+
+	/**
 	 * Start a timer with the given name
 	 */
-	Analytics.prototype.startTimer = function(name) {
+	Analytics_prototype.startTimer = function(name) {
 		// If timer is already started, don't start
 		if (this.timers[name] !== undefined) return;
 		// Store the current time in the given timer
 		this.timers[name] = Date.now();
-		this.timerAccummulators[name] = 0;
+		this.timerAccumulators[name] = 0;
 	}
 
 	/**
 	 * Restart a timer with the given name
 	 */
-	Analytics.prototype.restartTimer = function(name) {
+	Analytics_prototype.restartTimer = function(name) {
 		// If we already have a timer, get current duration
 		var duration = this.stopTimer(name);
 		// Replace timer start time
 		this.timers[name] = Date.now();
-		this.timerAccummulators[name] = 0;
+		this.timerAccumulators[name] = 0;
 		// Return duration
 		return duration;
 	}
@@ -271,28 +299,200 @@
 	/**
 	 * Return the time on the specified timer
 	 */
-	Analytics.prototype.getTimer = function(name) {
+	Analytics_prototype.getTimer = function(name) {
 		// Check for invalid timers
 		if (!this.timers[name]) return 0;
 		// Return duration
-		return  (Date.now() - this.timers[name]) + this.timerAccummulators[name];
+		return  (Date.now() - this.timers[name]) + this.timerAccumulators[name];
 	}
 
 	/**
 	 * Stop a timer with the given name and return
 	 * the time spent.
 	 */
-	Analytics.prototype.stopTimer = function(name) {
+	Analytics_prototype.stopTimer = function(name) {
 		// Check for invalid timers
 		if (!this.timers[name]) return 0;
 		// Stop timer and get duration
-		var duration = (Date.now() - this.timers[name]) + this.timerAccummulators[name];
+		var duration = (Date.now() - this.timers[name]) + this.timerAccumulators[name];
 		delete this.timers[name];
-		delete this.timerAccummulators[name];
+		delete this.timerAccumulators[name];
 		// Return duration
 		return duration;
 	}
 
+	/* ############################################################ */
+	/*  Accumulators API Functions                                  */
+	/* ############################################################ */
+
+	/**
+	 * Trigger as many events as needed in order to send even invremental
+	 * intervals for the specified property.
+	 */
+	Analytics_prototype.fireIncrementalEvent = function( eventName, data, config, value ) {
+		var cfg = {
+			'property'	: null,
+			'dataName'	: null,
+			'interval'	: 1,
+			'value'		: 1
+		};
+
+		// Set default data
+		var data = data || { };
+
+		// Handle string or object config
+		if (typeof(config) == 'string') {
+			cfg.property = config;
+			cfg.dataName = config;
+		} else {
+			cfg.property = config['property'];
+			cfg.dataName = config['dataName'] || config['property'];
+			cfg.interval = config['interval'] || 1;
+			cfg.value 	 = config['value'] || 1;
+		}
+		if (value !== undefined) {
+			cfg.value = value;
+		}
+
+		// Load incremental data
+		var incremental = {};
+		if (localStorage.hasOwnProperty(KEY_INCREMENTAL))
+			incremental = JSON.parse( localStorage.getItem(KEY_INCREMENTAL) );
+
+		// Get last incremental value
+		if (!incremental[cfg.property])
+			incremental[cfg.property] = 0;
+		var lastValue = incremental[cfg.property];
+
+		// Calculte current incremental value
+		var currValue = Math.floor( cfg.value / cfg.interval ) * cfg.interval;
+
+		// Continue only if new value is bigger
+		if (currValue > lastValue) {
+
+			// Fire last to current intervals
+			for (var v=lastValue+cfg.interval; v<=currValue; v+=cfg.interval) {
+				// Update event values
+				data[ cfg.dataName ] = v;
+				// Trigger event
+				this.fireEvent( eventName, data );
+			}
+
+			// Update accumulator delta value
+			incremental[cfg.property] = currValue;
+			localStorage.setItem(KEY_INCREMENTAL, JSON.stringify(incremental));
+
+		}
+
+	}
+
+	/**
+	 * Clear accumulator under given name
+	 */
+	Analytics_prototype.clearAccum = function( name ) {
+
+		// Load Accumulators
+		var accumulators = {};
+		if (localStorage.hasOwnProperty(KEY_ACCUMULATORS))
+			accumulators = JSON.parse( localStorage.getItem(KEY_ACCUMULATORS) );
+
+		// Delete
+		if (accummulators[name] !== undefined)
+			delete accummulators[name];
+
+		// Update item
+		localStorage.setItem(KEY_ACCUMULATORS, JSON.stringify(accumulators));
+
+	}
+
+	/**
+	 * Clear all local counters
+	 */
+	Analytics_prototype.clearAll = function() {
+
+		// Remove local storage items
+		localStorage.removeItem( KEY_ACCUMULATORS );
+		localStorage.removeItem( KEY_ACCUMDELTA );
+		localStorage.removeItem( KEY_INCREMENTAL );
+
+	}
+
+	/**
+	 * Accumulate and return a value for the given metric name
+	 * 
+	 * Store the 'value' in the accummulator with name 'name' and
+	 * return it's value.
+	 */
+	Analytics_prototype.accumulate = function( name, value ) {
+
+		// Load Accumulators
+		var accumulators = {};
+		if (localStorage.hasOwnProperty(KEY_ACCUMULATORS))
+			accumulators = JSON.parse( localStorage.getItem(KEY_ACCUMULATORS) );
+
+		// Update accumulator
+		if (!accumulators[name]) {
+			accumulators[name] = value;
+		} else {
+			accumulators[name] += value;
+		}
+
+		// Update item
+		localStorage.setItem(KEY_ACCUMULATORS, JSON.stringify(accumulators));
+
+		// Return value
+		return accumulators[name];
+
+	}
+
+	/**
+	 * Clear delta value under given name
+	 */
+	Analytics_prototype.clearDelta = function( name ) {
+
+		// Load Accumulators
+		var accumulators = {};
+		if (localStorage.hasOwnProperty(KEY_ACCUMDELTA))
+			accumulators = JSON.parse( localStorage.getItem(KEY_ACCUMDELTA) );
+
+		// Delete
+		if (accummulators[name] !== undefined)
+			delete accummulators[name];
+
+		// Update item
+		localStorage.setItem(KEY_ACCUMDELTA, JSON.stringify(accumulators));
+
+	}
+
+	/**
+	 * Calculate the delta in the value from the current value of
+	 * value and the last one stored.
+	 *
+	 * This function retuns the delta and updates the stored value
+	 * for the specified name.
+	 */
+	Analytics_prototype.delta = function( name, value ) {
+
+		// Get last deltas
+		var accumDelta = {};
+		if (localStorage.hasOwnProperty(KEY_ACCUMDELTA))
+			accumDelta = JSON.parse( localStorage.getItem(KEY_ACCUMDELTA) );
+
+		// Get last accumulator delta value
+		if (!accumDelta[name])
+			accumDelta[name] = 0;
+
+		// Calculate delta
+		var delta = value - accumDelta[name];
+
+		// Update delta store
+		accumDelta[name] = value;
+		localStorage.setItem(KEY_ACCUMDELTA, JSON.stringify(accumDelta));
+
+		// Update accumulator
+		return delta;
+
+	}
 
 	// Create and return an analytics instance
 	var analytics = new Analytics();
